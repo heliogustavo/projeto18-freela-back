0dc8b8be3cdbfe98205b003675d64fa43b9781d9
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2024-02-08 14:22:40 -0400

    Erros de schemas corrigidos

--- src/routes/cat.routes.js
+++ src/routes/cat.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getMiaudelos, addMiaudelo } from "../controllers/cat.controllers.js"
+import { getMiaudelos, addMiaudelo } from "../controllers/url.controllers.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { photoLinkSchema } from "../schemas/cats.schemas.js"
 import { validateAuth } from "../middlewares/validateAuth.js"
@@ -7,6 +7,6 @@ import { validateAuth } from "../middlewares/validateAuth.js"
 const catsRouter = Router()
 
 catsRouter.post("/addMiaudelo", validateSchema(photoLinkSchema), validateAuth, addMiaudelo)
-catsRouter.get("/miaudelosList/:id", getMiaudelos)
+catsRouter.get("/miaudelo/:id", getMiaudelos)
 
 export default catsRouter
