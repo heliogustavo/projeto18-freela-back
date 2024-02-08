a6f16c2a4fa2c2f9be854a8d6b152d4eed3909fc
heliogustavo <jacomehelioiq@gmail.comgit config --global user.name heliogustavogit config --global user.email jacomehelioiq@gmail.com>
2024-02-08 14:12:30 -0400

    schemas, cronctrollers de usuario e rotas criado

--- src/routes/cat.routes.js
+++ src/routes/cat.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express"
 import { getMiaudelos, addMiaudelo } from "../controllers/cat.controllers.js"
 import { validateSchema } from "../middlewares/validateSchema.js"
-import { photoLinkSchema } from "../schemas/url.schemas.js"
+import { photoLinkSchema } from "../schemas/cats.schemas.js"
 import { validateAuth } from "../middlewares/validateAuth.js"
 
-const urlRouter = Router()
+const catsRouter = Router()
 
-urlRouter.post("/addMiaudelo", validateSchema(photoLinkSchema), validateAuth, addMiaudelo)
-urlRouter.get("/miaudelosList/:id", getMiaudelos)
+catsRouter.post("/addMiaudelo", validateSchema(photoLinkSchema), validateAuth, addMiaudelo)
+catsRouter.get("/miaudelosList/:id", getMiaudelos)
 
-export default urlRouter
+export default catsRouter
